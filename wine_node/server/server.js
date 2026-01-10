926fb3c27435243e7d6051e0e7e0b3429f83c0cb
Trevor-Y2004 <tyoung2204@gmail.com>
2026-01-09 16:44:33 -0800

    node.js server now communicates with an mqtt broker

--- wine_node/server/server.js
+++ wine_node/server/server.js
@@ -1,5 +1,36 @@
 const express = require('express');
+const mqtt = require('mqtt');
+
 const app = express();
+const PORT = 3000;
+//replace ip with the ip of your machine on the LAN
+const MQTT_BROKER_URL = 'mqtt://192.168.86.34:1883';
+//const MQTT_BROKER_URL = 'mqtt://mqtt:1883';
+
+const client = mqtt.connect(MQTT_BROKER_URL);
+
+//connect to mqtt broker
+client.on('connect', () => {
+    console.log('I connected')
+    client.subscribe('iot/telemetry', (e) => {
+        if (e) {
+            console.error(e);
+        }
+    });
+
+});
+
+//listen for messages
+client.on('message', (topic, message) => {
+    //pico passes an encoded byte-string 
+    const payload = JSON.parse(message.toString('utf-8'));
+
+    console.log(`Recieved message on ${topic} from ${payload.deviceID}: tempF=${payload.tempF}`);
+});
+
+
+//-----------everything below this is express and http - needs substantial iteration-------
+
 app.use(express.json()); //data will be sent as json objects from the pico to the server
 
 app.get('/', (req, res) => {
